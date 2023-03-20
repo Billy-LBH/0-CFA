96a17fc776c66e47b1cd056739b44a1a3d3abed9
Billy-LBH <mj20403@bristol.ac.uk>
2023-03-20 15:20:39 +0000

    0-cfa done

--- parser/src/parser.erl
+++ parser/src/parser.erl
@@ -5,9 +5,19 @@
 
 -module(parser).
 
--export([parse/1, newlabel/1, getLambdas/2, congen/2, recall/3]).
+-export([cfa/1]).
 
 % E ::= x | (E1 E2) | (/x.E)
+
+cfa(Input) ->
+    Ptree = parse(Input),
+    LabelledPtree = newlabel(Ptree),
+    Map = newmap(Ptree),
+    Lams = getLambdas(LabelledPtree, []),
+    Conlist = congen(LabelledPtree, Lams),
+    solve(Conlist, Map).
+
+% -------------------- Parse ------------------ %
 
 parse(Input) ->
     {Expr, Rest} = parse_expr(Input),
@@ -92,6 +102,12 @@ newlabel(Tree) ->
     {LabelledTree, _} = label(Tree, 1),
     LabelledTree.
 
+newmap(Tree) ->
+    {_, N} = label(Tree, 1),
+    Keys = lists:seq(1, N - 1),
+    KeyValueTuples = [{Key, []} || Key <- Keys],
+    maps:from_list(KeyValueTuples).
+
 % label(Tree, N) ->
 %     case Tree of
 %         {var, Var} ->
@@ -143,25 +159,119 @@ getLambdas({_, _, _}, FinalList) ->
     FinalList.
 
 congen({var, L, Var}, _) ->
-    [{flow, Var, L}];
+    [{flow, {var, Var}, L}];
 congen({lam, L, V, E}, Lams) ->
     Cs = congen(E, Lams),
     [{flow, {lam, L, V, E}, L} | Cs];
 congen({app, L, E1, E2}, Lams) ->
     C1 = congen(E1, Lams),
     C2 = congen(E2, Lams),
-    recall(Lams, L, E2) ++ C1 ++ C2.
+    recall(Lams, L, E1, E2) ++ C1 ++ C2.
 
-recall([], _, _) ->
+recall([], _, _, _) ->
     [];
-recall([F | Rest], L, E2) ->
-    LE1 = element(2, F),
+recall([F | Rest], L, E1, E2) ->
+    LE1 = element(2, E1),
     LE2 = element(2, E2),
-    LV1 = element(3, F),
+    LV1 = element(2, lists:nth(1, element(3, F))),
     LE = element(2, element(4, F)),
     C1 = {implies, {flow, F, LE1}, {flow, LE2, LV1}},
-    C2 = {implies, {flow, F, LE1}, {flow, LE, LE1}},
-    [C1, C2 | recall(Rest, L, E2)].
+    C2 = {implies, {flow, F, LE1}, {flow, LE, L}},
+    [C1, C2 | recall(Rest, L, E1, E2)].
+
+process_flow(From, To, Map) ->
+    case From of
+        {lam, L, V, E} ->
+            Fun = fun(I) -> lists:uniq(I ++ [{lam, L, V, E}]) end,
+            Newmap = maps:update_with(To, Fun, [{lam, L, V, E}], Map);
+        {var, Var} ->
+            Fun = fun(I) -> lists:uniq(I ++ [Var]) end,
+            Newmap = maps:update_with(To, Fun, [Var], Map);
+        Label ->
+            Fun = fun(I) -> lists:uniq(I ++ maps:get(Label, Map)) end,
+            Newmap = maps:update_with(To, Fun, maps:get(Label, Map), Map)
+    end,
+    Newmap.
+
+process_imp(F1, F2, Map) ->
+    case element(2, F1) of
+        {lam, L, V, E} ->
+            case lists:member({lam, L, V, E}, maps:get(element(3, F1), Map)) of
+                true ->
+                    process_flow(element(2, F2), element(3, F2), Map);
+                false ->
+                    Map
+            end;
+        {var, Var} ->
+            case lists:member(Var, maps:get(element(3, F1), Map)) of
+                true ->
+                    process_flow(element(2, F2), element(3, F2), Map);
+                false ->
+                    Map
+            end;
+        Label ->
+            case lists:subset(maps:get(Label), maps:get(element(3, F1), Map)) of
+                true ->
+                    process_flow(element(2, F2), element(3, F2), Map);
+                false ->
+                    Map
+            end
+    end.
+
+solve_aux([{flow, From, To} | Rest], Map) ->
+    Newmap = process_flow(From, To, Map),
+    solve_aux(Rest, Newmap);
+solve_aux([{implies, F1, F2} | Rest], Map) ->
+    Newmap = process_imp(F1, F2, Map),
+    solve_aux(Rest, Newmap);
+solve_aux([], Map) ->
+    Map.
+
+solve(ConList, Map) ->
+    Newmap = solve_aux(ConList, Map),
+    case Newmap == Map of
+        true ->
+            Map;
+        false ->
+            solve(ConList, Newmap)
+    end.
+
+% check(T1, T2) ->
+%     case T2 of
+%         {flow, _, _} when element(3, T1) == element(3, T2) ->
+%             [T2];
+%         _ ->
+%             []
+%     end.
+
+% flowfilter([First | Rest]) ->
+%     lists:map(
+%         fun(T) -> check(First, T) end,
+%         Rest
+%     ).
+
+% solve([First | Rest]) ->
+%     List = [],
+%     case First of
+%         {flow, T1, T2} ->
+%             [{T2}, {T1}];
+%         _ ->
+%             []
+%     end,
+%     List ++ solve(Rest);
+% solve([]) ->
+%     [].
+
+% solve([First | Rest]) ->
+%     case First of
+%         {flow, T1, T2} ->
+%             [{T2}, {T1}];
+%         {implies, F1, F2} when lists:member(F1, Flowlist)->
+
+        
+
+
+
 
 %     C1 = congen(E1),
     %     C2 = congen(E2),
@@ -195,5 +305,7 @@ recall([F | Rest], L, E2) ->
     % end.
 
 % use uniq to avoid duplicated situation
+
+% update_with function
 
 %% internal functions
